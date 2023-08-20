c85a638c7bf483b48459b6bc1fc238949be8fb5c
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2023-08-20 18:04:05 -0400

    detecting hand

--- app/components/signDetection/SignAndCamera.tsx
+++ app/components/signDetection/SignAndCamera.tsx
@@ -7,14 +7,38 @@ import * as handpose from "@tensorflow-models/handpose";
 import "@tensorflow/tfjs-backend-webgl";
 
 const SignAndCamera = () => {
-  const webcamRef = useRef(null);
+  const webcamRef = useRef<Webcam>(null);
 
   const runHandpose = async () => {
     try {
       const net = await handpose.load();
       console.log({ net }, "Handpose model loaded");
+      setInterval(() => {
+        detect(net);
+      }, 100);
     } catch (e) {
       console.log({ e });
+    }
+  };
+
+  const detect = async (net) => {
+    const webcam = webcamRef.current;
+    if (
+      typeof webcam !== "undefined" &&
+      webcam !== null &&
+      webcam.video?.readyState === 4
+    ) {
+      //Set video properties
+      let {
+        video,
+        video: { videoWidth, videoHeight, width, height },
+      } = webcam;
+      width = videoWidth;
+      height = videoHeight;
+
+      //Make detections
+      const hand = await net.estimateHands(video);
+      //console.log(hand);
     }
   };
 
